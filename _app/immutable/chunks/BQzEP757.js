5cbbcb365f30d9dda5b9db771b225f36646153be
Alik Zapolnov <alikzapolnov@gmail.com>
2025-07-16 20:18:49 +0300

    Updates

--- _app/immutable/chunks/BQzEP757.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_obbcl8?.base??"/Stay_on_the_board",a=globalThis.__sveltekit_obbcl8?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/Zl5nt9oV.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1yvo8aj?.base??"/Stay_on_the_board",a=globalThis.__sveltekit_1yvo8aj?.assets??s;export{a,s as b};
